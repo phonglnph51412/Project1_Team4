99436de7a96209b9f7edddbb70ec3a2987258db9
Phongln <phonglnph51412@gmail.com>
2024-11-15 15:57:00 +0700

    env

--- commons/env.php
+++ commons/env.php
@@ -3,12 +3,12 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL'       , 'http://localhost/base-thi-mvc-oop/mvc-oop-basic//');
+define('BASE_URL'       , 'http://localhost/Duan1\Project1_Team4//');
 
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME'    , 'phongln');  // Tên database
+define('DB_NAME'    , 'Duan1');  // Tên database
 
 define('PATH_ROOT'    , __DIR__ . '/../');
